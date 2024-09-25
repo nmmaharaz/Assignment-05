e5e44a3b1a4e4535b3bad4d27259dacd40383dd0
nmmaharaz <nmmaharaz@gmail.com>
2024-09-25 12:33:24 +0600

    complete Donate Bangladesh Website

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,3 @@
 document.getElementById('blog').addEventListener('click', function(event){
       window.location.href='./blog.html'
 })
-
-
--- js/utilities1.js
+++ js/utilities1.js
@@ -2,19 +2,16 @@ function getInputElementById (id){
       const inputFlied = document.getElementById(id).value
       const inputAmount = parseFloat(inputFlied)
       return inputAmount
-      // console.log(inputAmount)
 }
 
 function getTotalDonationElementById (id){
       const totalAmount = document.getElementById(id).innerText
       const totalDonation = parseFloat(totalAmount)
       return totalDonation
-      // console.log(totalDonation)
 }
 
 function getAllTotalDonationAmountById (id){
       const allTotalDonation = document.getElementById(id).innerText
       const allTotalDon = parseFloat(allTotalDonation)
       return allTotalDon
-      // console.log(allTotalDon)
 }
